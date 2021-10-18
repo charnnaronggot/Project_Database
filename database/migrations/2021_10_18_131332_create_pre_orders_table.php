4e82ee1b4065ecc33547a4ba7050e75730edad46
phumiwit <68888298+phumiwit@users.noreply.github.com>
2021-10-18 23:39:32 +0700

    Add files via upload

---
+++ database/migrations/2021_10_18_131332_create_pre_orders_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePreOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('pre_orders', function (Blueprint $table) {
+            $table->id();
+            $table->integer('orderNumber')->unique();
+            $table->integer('productCode')->unique();
+            $table->double('preSale');
+            $table->quantity('quantity');
+            $table->softDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('pre_orders');
+    }
+}
---
+++ database/migrations/2021_10_18_133327_create_product_lines_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateProductLinesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('product_lines', function (Blueprint $table) {
+            $table->id();
+            $table->integer('productLine')->unique();
+            $table->string('textDescription');
+            $table->string('htmlDescription');
+            $table->string('imageBL');
+            $table->softDelete();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('product_lines');
+    }
+}
